b5b3db928344606d448bd0b2bfff7d54fcdd26da
manon <mmansveld@roc-dev.com>
2019-10-08 14:21:43 +0200

    InleidingJava

---
+++ src/h04/Dobbelsteen.java
@@ -0,0 +1,24 @@
+
+import java.awt.*;
+        import java.applet.*;
+
+public class Dobbelsteen
+        extends Applet {
+
+    public void init() {
+        setBackground(Color.lightGray);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRoundRect(110,60,400,400, 45, 45);
+
+        g.setColor(Color.black);
+        g.fillOval(180, 125, 75, 75);
+        g.fillOval(180, 325, 75, 75);
+        g.fillOval(370, 325, 75, 75);
+        g.fillOval(370, 125, 75, 75);
+
+
+    }
+}
---
+++ src/h04/Stoplicht.java
@@ -0,0 +1,29 @@
+
+import java.awt.*;
+        import java.applet.*;
+
+public class Stoplicht
+        extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.black);
+        g.fillRoundRect(20,20,200,400,45,45);
+
+        g.setColor(Color.lightGray);
+        g.fillRect(105,420, 30,400);
+
+        g.setColor(Color.red);
+        g.fillOval(75, 65, 90, 90);
+
+        g.setColor(Color.orange);
+        g.fillOval(75, 175, 90, 90);
+
+        g.setColor(Color.green);
+        g.fillOval(75, 285, 90, 90);
+
+    }
+}
