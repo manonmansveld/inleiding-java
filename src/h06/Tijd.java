6211286c692c296bf4f4d49c318c3fa0d194ed4b
manon <mmansveld@roc-dev.com>
2019-10-29 11:29:59 +0100

    InleidingJava

--- src/h06/Tijd.java
+++ src/h06/Tijd.java
@@ -5,26 +5,26 @@ package h06;
 
 
 public class Tijd extends Applet {
-    int Seconde;
-    int Uur;
-    int Dag;
-    int Jaar;
+    int seconde;
+    int uur;
+    int dag;
+    int jaar;
 
 
 
     public void init() {
         setBackground(Color.darkGray);
-        Seconde = 60;
-        Uur = 60;
-        Dag = 24;
-        Jaar = 365;
+        seconde = 60;
+        uur = 60;
+        dag = 24;
+        jaar = 365;
     }
 
     public void paint(Graphics g) {
         g.setColor(Color.white);
         g.setFont(new Font("Century Gothic", Font.PLAIN, 20));
-        g.drawString("Eén uur heeft   " + Seconde * Uur, 20, 20);
-        g.drawString("Eén uur heeft   " + Seconde * Uur * Dag,  20, 40);
-        g.drawString("Eén jaar heeft  " + Seconde * Uur * Dag * Jaar, 20, 60);
+        g.drawString("Eén uur heeft   " + seconde * uur, 20, 20);
+        g.drawString("Eén uur heeft   " + seconde * uur * dag,  20, 40);
+        g.drawString("Eén jaar heeft  " + seconde * uur * dag * jaar, 20, 60);
     }
 }
