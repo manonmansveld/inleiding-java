34d63aad89db25e3c8fd79be042d9169a3f1a6ee
manon <mmansveld@roc-dev.com>
2020-06-19 10:12:37 +0200

    Commit

--- src/h13/Opdracht2.java
+++ src/h13/Opdracht2.java
@@ -1,4 +1,39 @@
 package h13;
 
-public class Opdracht2 {
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht2 extends Applet {
+
+
+    public void init() {
+        setSize(650, 650);
+        setBackground(Color.lightGray);
+
+    }
+
+    public void paint(Graphics g) {
+
+        tekenMuur(g);
+
+    }
+
+    void tekenMuur(Graphics g){
+
+        int x, y;
+
+        for (int rijx = 0; rijx < 5; rijx++) {
+            for (int rijy = 0; rijy < 5; rijy++){
+                x = rijx * 55;
+                y = rijy * 25;
+
+
+                g.setColor(Color.red);
+                g.fillRect(x, y, 50, 20);
+            }
+
+        }
+
+    }
+
 }
